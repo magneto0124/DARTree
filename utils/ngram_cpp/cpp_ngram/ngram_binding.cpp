a4743600dfbc03954d185b51d71adcae67d252ec
linmingqian <1519095211@qq.com>
2026-09-14 11:48:53 +0800

    [Fix] ngram cpp compilation bug

--- utils/ngram_cpp/cpp_ngram/ngram_binding.cpp
+++ utils/ngram_cpp/cpp_ngram/ngram_binding.cpp
@@ -3,7 +3,12 @@
 
 #include "../inc/ngram.h"
 
-PYBIND11_MODULE(ngram_cpp, m) {
+// Module name follows torch's TORCH_EXTENSION_NAME macro (defined by
+// torch.utils.cpp_extension.load(name="dartree_ngram_cpp")), so the exported
+// PyInit_* symbol matches the extension name. DART upstream hardcodes
+// "ngram_cpp" here and uses name="ngram_cpp" in load(); DARTree renames the
+// extension to avoid clobbering DART's build cache, hence the macro form.
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   using namespace ngram;
   namespace py = pybind11;
   m.doc() = "";
